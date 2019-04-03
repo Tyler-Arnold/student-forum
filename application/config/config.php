e85a655b68da9c1c4a17d47d2591c59971aa2ff2
Tyler Arnold <arnoldtyler1998@gmail.com>
2019-04-04 00:58:36 +0100

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://student40080.bucomputing.uk/student-forum/';
+$config['base_url'] = 'http://student40081.bucomputing.uk/student-forum/';
 
 /*
 |--------------------------------------------------------------------------
