fd243b237908ed4578e49174eb726771e34022b6
Lee Barnes <barneslee306@gmail.com>
2019-04-04 21:07:26 +0100

    Push origin
    new commit

--- application/controllers/Feed.php
+++ application/controllers/Feed.php
@@ -2,45 +2,22 @@
 class Feed extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->model('feed_model'); // load feed model
+        $this->load->model('feed_model'); // load our custom model
         $this->load->model('calendar_model'); // load our custom model
-        $this->load->library('session'); 
+        $this->load->library('session');
     }
 
     public function index() {
+        $data['user'] = $this->session->userdata('id');
         $data['messages'] = $this->feed_model->get_messages();
-        $data['calendar'] = $this->calendar_model->get_calendar(4);
+        $data['title'] = "Your Feed"; // Capitalize the first letter
 
-        $data['title'] = "Your Feed"; // page title
-        $data['title'] = "Your Feed"; // Title
-        $userid=$this->session->userdata('id');
-		
-        if(isset($userid)){
-            $data['messages'] = $this->feed_model->get_messages();
-            $data['title'] = "Your Feed"; // Capitalize the first letter
-            $this->load->helper('form'); // form helper functions, used in the create view
-            $this->load->library('form_validation'); // load form validation library
+        $this->load->helper('form'); // load form helper
+		$this->load->view('templates/header', $data); //load header
+		$this->load->view('pages/message-box', $data); //load message box
+        $this->load->view('pages/feed', $data); //load message box
+		$this->load->view('templates/footer', $data); //load footer
 
-            $this->form_validation->set_rules('recipient', 'Recipient', 'required');
-            $this->form_validation->set_rules('message', 'Message', 'required');
-
-            $this->load->view('templates/header', $data); //load header
-            $this->load->view('pages/calendar', $data); //load calendar
-
-            if ($this->form_validation->run() === FALSE) { // form failed validation
-              $this->load->view('pages/message-box', $data); //load message box
-            } else { // form validated successfully
-              $this->load->view('pages/message-box', $data); //load message box
-              $this->feed_model->send_message(); // call function in model to put validated data into database
-              redirect($this->uri->uri_string());
-            }
-
-                $this->load->view('pages/feed', $data); //load feed
-            $this->load->view('templates/footer', $data); //load footer
-            } else {
-              redirect('user/login','refresh');
-          }
-
-      }
+    }
 
 }
