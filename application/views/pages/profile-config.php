36949a0cd98dff9956f69f55a90d544fc0836fdf
Tyler Arnold <ArnoldTyler1998@gmail.com>
2019-04-05 07:26:38 +0100

    Added user status to search results

--- application/views/pages/profile-config.php
+++ application/views/pages/profile-config.php
@@ -1,14 +1,15 @@
 <div class="user-profile">
 	<div class="profile-body"></div>
 
-        <img class="profile_picture" src="<?php echo base_url(); ?>images/<?php echo $profile_pic; ?>" height="350px" width="350px" /><br><br> 
+        <img class="profile_picture" src="<?php echo base_url(); ?>images/<?php echo $profile_pic; ?>" height="350px" width="350px" /><br><br>
 
     <B><p>My Role:</p></B><?php echo $status; ?>
     <br><br>
 
     <?php echo validation_errors(); ?>
     <?php echo form_open_multipart('profile/submit_form');?>
-
+	<p>Max filesize: 2MB</p>
+	<p>Formats accepted: JPG, PNG</p>
     <input type="file" name="profile_picture">
 
     <b><p>My Bio:</p></b>
--- application/views/search/results.php
+++ application/views/search/results.php
@@ -4,47 +4,47 @@
 		<?php echo $errormsgs; ?>
 		<?php foreach ($resultmsgs as $resultmsg): ?>
 			<div class="feed-message">
-				<h3 class="sender"><a href="<?php echo base_url("profile/index/".$resultmsg['sender']); ?>"><?php echo $resultmsg['username']; ?></a></h3>
+				<h3 class="sender"><a href="<?php echo base_url("profile/index/".$resultmsg['sender']); ?>"><?php echo $resultmsg['username']." - ".$resultmsg['status']; ?></a></h3>
 				<div class="message-body">
 					<p><?php echo $resultmsg['message_body']; ?></p>
 				</div>
 
 				<div class="message-interaction">
 					 <a href="<?php echo base_url("feed/thread/".$resultmsg['id']);?>"><button>Reply</button></a>
-					
+
 				</div>
 			</div>
 		<?php endforeach; ?>
 	</div>
-	
+
 	<div class="item2">
 	<h1> Sent Messages </h1>
 		<?php echo $errorsent; ?>
 		<?php foreach ($resultsent as $resultsent1): ?>
 			<div class="feed-message">
-				<h3 class="sender"><a href="<?php echo base_url("profile/index/".$resultsent1['sender']); ?>"><?php echo $resultsent1['username']; ?></a></h3>
+				<h3 class="sender"><a href="<?php echo base_url("profile/index/".$resultsent1['sender']); ?>"><?php echo $resultsent1['username']." - ".$resultsent1['status']; ?></a></h3>
 				<div class="message-body">
 					<p><?php echo $resultsent1['message_body']; ?></p>
 				</div>
 
 				<div class="message-interaction">
 					 <a href="<?php echo base_url("feed/thread/".$resultsent1['id']);?>"><button>Reply</button></a>
-					
+
 				</div>
 			</div>
 		<?php endforeach; ?>
 	</div>
-	
+
 	<div class="item3">
 	<h1> Users </h1>
 		<?php echo $errorusers; ?>
 		<?php foreach ($resultusers as $resultuser): ?>
 			<div class="feed-message">
-				
-				<h3 class="sender"><a href="<?php echo base_url("profile/index/".$resultuser['id']); ?>"><?php echo $resultuser['username']; ?></a></h3>
+
+				<h3 class="sender"><a href="<?php echo base_url("profile/index/".$resultuser['id']); ?>"><?php echo $resultuser['username']." - ".$resultuser['status']; ?></a></h3>
 			</div>
 		<?php endforeach; ?>
-		
+
 	</div>
 
 
