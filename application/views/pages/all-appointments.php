3eef34b5f62b8eedc4f5cebe8da102cd3f458098
Tyler Arnold <ArnoldTyler1998@gmail.com>
2019-04-05 06:42:06 +0100

    Users can no longer accept and reject appointments they sent

--- application/views/pages/all-appointments.php
+++ application/views/pages/all-appointments.php
@@ -4,7 +4,7 @@
         <p><em>Date-Time:</em> <?php echo $appointment['date_time'] ?></p>
 		<p><em>Location:</em> <?php echo $appointment['location'] ?></p>
         <p><em>Status:</em> <?php echo $appointment['status'] ?></p>
-        <?php if($appointment['status'] != "accepted"): ?>
+        <?php if($appointment['status'] != "accepted" && $appointment['sender'] != $user): ?>
         <a href="<?php echo base_url("appointments/update/{$appointment['id']}/accepted"); ?>" ><button>Accept</button></a>
         <a href="<?php echo base_url("appointments/update/{$appointment['id']}/rejected"); ?>" ><button>Reject</button></a>
         <?php endif; ?>
